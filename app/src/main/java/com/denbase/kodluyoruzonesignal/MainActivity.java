b60c58ea7787bcb6bfaa66ff668e6d06dbef801c
Emre <emre10sahin10@gmail.com>
2020-12-26 15:45:38 +0300

    Initial commit

---
+++ app/src/main/java/com/denbase/kodluyoruzonesignal/MainActivity.java
@@ -0,0 +1,24 @@
+package com.denbase.kodluyoruzonesignal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.onesignal.OneSignal;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+
+        // OneSignal Initialization
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+
+    }
+}
